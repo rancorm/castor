fc2c558eeb021377f80eee189d79e0ce70d656de
Jonathan Cormier <jonathan@cormier.co>
2024-02-14 11:10:59 -0400

    Project logo, content view, environment variables
    Add project logo image file.
    Add CastorContentView class for rending JSON as schema.
    Update README with environment options details.
    Convert Castor._generate_schema function to global.
    Update Castor.response to attempt JSON decodes before using text.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Castor
 
+<p align="center">
+    <img src="https://raw.githubusercontent.com/rancorm/castor/main/beaver.png" width="200" height="200" alt="Castor logo" />
+</p>
+
+Image is sourced from [pngimg](https://pngimg.com/image/31353).
+
 mitmproxy add-on that captures JSON schemas.
 
 ## Start here
@@ -8,6 +14,7 @@ Clone this repository.
 
 ```
 git clone https://github.com/rancorm/castor.git
+cd castor
 ```
 
 Make Python virtual environment, install dependencies, and run.
@@ -17,11 +24,30 @@ python -m venv .venv
 pip install -r requirements.txt
 ```
 
-Run mitmproxy or mitmweb with the add-on script (-s).
+Run `mitmproxy` or `mitmweb` with the add-on script `castor.py`.
 
 ```
-mitmweb -s ./castor.py
+mitmproxy -s ./castor.py
+```
+
+```
+mitmweb -s ./castor.py --no-web-open-browser
 ```
 
 ## Output
 
+
+## Options
+
+Disable checks of content type, set `CASTOR_CHECKS` to `0`. To disable the
+auto rendering of JSON to JSON schema, set `CASTOR_AUTO_RENDER` to `1`.
+
+## Saving to disk
+
+To save generated schema to disk, set `CASTOR_OUTPUT` to the path for saving.
+
+```
+export CASTOR_OUTPUT=/mnt/data
+```
+
+This can perticularly be useful with persisent storage volumes.
--- castor.py
+++ castor.py
@@ -5,101 +5,213 @@ Run as follows:
     mitmproxy -s castor.py
 or 
     mitmweb -s castor.py
+
+Set environment variable CASTOR_AUTO_RENDER=1 to enable automatic rendering of JSON schemas.
+Set environment variable CASTOR_CHECKS=0 to disable request/response content type checks.
+Set environment variable CASTOR_OUTPUT=/path/to/json/schemas/ to enable output to file system.
 """
-import logging
 import json
+import os
+
+from mitmproxy import contentviews
+from mitmproxy import ctx
+from mitmproxy import flow
+from mitmproxy import http
+from mitmproxy.addonmanager import Loader
+
+def generate_schema(json_object):
+    schema = { "type": "object", "properties": {} }
+
+    # Handle simple types 
+    if type(json_object) is not dict and type(json_object) is not list:
+       return type(json_object).__name__.lower()
+
+    if type(json_object) is list:
+        # Assume that all elements in the array have the same schema
+        for item in json_object:
+            return generate_schema(item) if item else {}
+
+    # Generate schema for each key in the JSON object
+    for key, value in json_object.items():
+        if isinstance(value, dict):
+            # Recursively generate schema for nested objects
+            schema["properties"][key] = generate_schema(value)
+        elif isinstance(value, list):
+            # Handle arrays
+            if value:
+                # Assume that all elements in the array have the same schema
+                schema["properties"][key] = {
+                    "type": "array",
+                    "items": generate_schema(value[0]) if value else {}
+                }
+            else:
+                # Empty array, no specific items schema
+                schema["properties"][key] = { "type": "array" }
+        else:
+            # Simple type
+            schema["properties"][key] = { "type": type(value).__name__.lower() }
+
+    return schema
+
+class CastorContentView(contentviews.View):
+    name = "json-schema"
+    content_types = [ "application/json" ]
+    auto_render = os.getenv("CASTOR_AUTO_RENDER", 0)
+
+    def __call__(
+        self,
+        data: bytes,
+        *,
+        content_type: str | None = None,
+        flow: flow.Flow | None = None,
+        http_message: http.Message | None = None,
+        **unknown_metadata,
+    ) -> contentviews.TViewResult:
+        return "JSON schema", contentviews.format_text(json.dumps(generate_schema(flow.response.json()), indent=2) if flow else {})
+
+    def render_priority(
+        self,
+        data: bytes,
+        *,
+        content_type: str | None = None,
+        flow: flow.Flow | None = None,
+        http_message: http.Message | None = None,
+        **unknown_metadata,
+    ) -> float:
+        if content_type in self.content_types and self.auto_render:
+            return 1
+        else:
+            return 0
 
 class Castor:
     def __init__(self):
         self.schemas = []
+        self.output = os.getenv("CASTOR_OUTPUT", None)
+        self.checks = os.getenv("CASTOR_CHECKS", 1)
 
-    def _generate_schema(self, json_object):
-        schema = { "type": "object", "properties": {} }
+    def _write_schema(self, schema, host, port, path, direction):
+        if self.output:
+            endpoint_path = path.replace("/", "_")
+            file_name = f"{host}_{port}{endpoint_path}.{direction}.json"
+            file_path = os.path.join(self.output, file_name)
 
-        # Handle simple types 
-        if type(json_object) is not dict:
-            schema["properties"]["type"] = type(json_object).__name__.lower()
-
-            return schema
-
-        # Generate schema for each key in the JSON object
-        for key, value in json_object.items():
-            if isinstance(value, dict):
-                # Recursively generate schema for nested objects
-                schema["properties"][key] = self._generate_schema(value)
-            elif isinstance(value, list):
-                # Handle arrays
-                if value:
-                    # Assume that all elements in the array have the same schema
-                    schema["properties"][key] = {
-                        "type": "array",
-                        "items": self._generate_schema(value[0]) if value else {}
-                    }
-                else:
-                    # Empty array, no specific items schema
-                    schema["properties"][key] = { "type": "array" }
-            else:
-                # Simple type
-                schema["properties"][key] = { "type": type(value).__name__.lower() }
-
-        return schema
+            ctx.log.info(f"Writing schema to {file_path}")
+            #with open(file_path, "w") as file:
+            #   json.dump(schema, file, indent=2)
 
     def response(self, flow):
+        # Flow request
         req = flow.request
         req_method = req.method
         req_host = req.host
         req_port = req.port
+        req_path = req.path
+        req_version = req.http_version
+
+        # Flow response
         resp = flow.response
         resp_code = resp.status_code
         resp_type = resp.headers.get("Content-Type", None)
-    
-        msg = f"{req_method} to {req_host}:{req_port}"
+        
+        msg = f""
 
-        if resp_type == "application/json":
+        # Handle requests only with JSON responses
+        if resp_type == "application/json" and self.checks:
             req_type = req.headers.get("Content-Type", None)
 
             if req_type:
-                msg += f" ({req_type})"
-
                 if req_type == "application/json":
                     try:
                         req_json = req.json()
-                        req_schema = self._generate_schema(req_json)
+                        req_schema = generate_schema(req_json)
                         req_msg = json.dumps(req_schema, indent=2)
 
                         msg += "\n" + req_msg
+
+                        if self.output:
+                            self._write_schema(req_msg,
+                                               req_host,
+                                               req_port,
+                                               req_path,
+                                               "req")
                     except json.JSONDecodeError:
-                        logging.error("Failed to parse JSON request")
-            else:
-                req_text = req.get_text()
+                        ctx.log.error("Failed to parse JSON request")
+                else:
+                    # No JSON request, try JSON decode, output text if fails
+                    try:
+                        req_json = req.json()
+                        req_schema = generate_schema(req_json)
+                        req_msg = json.dumps(req_schema, indent=2)
+                    except json.JSONDecodeError:
+                        ctx.log.error("Failed to parse JSON request, using text instead")
 
-                if req_text:
-                    msg += "\n" + req_text
-    
-        # Log request and text
-        logging.info(msg)
+                        req_msg = req.get_text()
 
-        msg = f"{resp_code} from {req_host}:{req_port}"
-        
-        if resp_type:
-            msg += f" ({resp_type})"
- 
-        # Log response and JSON
-        logging.info(msg)
+                    if req_msg:
+                        msg += "\n" + req_msg
+
+                        if self.output:
+                            self._write_schema(req_msg,
+                                               req_host,
+                                               req_port,
+                                               req_path,
+                                               "req")
+            else: 
+                # No content type, attempt JSON decode, output text if fails
+                try:
+                    req_json = req.json()
+                    req_schema = generate_schema(req_json)
+                    req_msg = json.dumps(req_schema, indent=2)
+                except json.JSONDecodeError:
+                    ctx.log.error("Failed to parse JSON request, using text instead")
+
+                    req_msg = req.get_text()
+
+                if req_msg:
+                    msg += "\n" + req_msg
+                    
+                    if self.output:
+                        self._write_schema(req_msg,
+                                           req_host,
+                                           req_port,
+                                           req_path,
+                                           "req")
+
+            # Log request and text
+            if msg:
+                ctx.log.info(f"---")
+                ctx.log.info(msg)
+                ctx.log.info(f"---")
 
         # Handle JSON responses
-        if resp_type == "application/json":
+        if resp_type == "application/json" and self.checks:
             try:
                 resp_json = resp.json()
 
                 if type(resp_json) is dict:
-                    resp_schema = self._generate_schema(resp_json)
+                    resp_schema = generate_schema(resp_json)
                     resp_msg = json.dumps(resp_schema, indent=2)
-                
-                    logging.info("\n" + resp_msg)
+               
+                    if resp_msg:
+                        ctx.log.info("\n" + resp_msg)
+                        ctx.log.info(f"---")
+
+                    if self.output:
+                        self._write_schema(resp_msg,
+                                           req_host,
+                                           req_port,
+                                           req_path,
+                                           "resp")
             except json.JSONDecodeError:
-                logging.error("Failed to parse JSON response")
+                ctx.log.error("Failed to parse JSON response")
 
                 return
 
+view = CastorContentView()
 addons = [Castor()]
+
+def load(loader: Loader):
+    contentviews.add(view)
+
+def done():
+    contentviews.remove(view)
